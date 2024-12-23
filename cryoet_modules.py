b6db58fab8487ad379b244959f27aaf8b4429a8c
hmhornung <85147335+hmhornung@users.noreply.github.com>
2024-12-22 20:53:32 -0600

    training

---
+++ cryoet_modules.py
@@ -0,0 +1,10 @@
+# master.py
+import sys
+import os
+
+# Add subfolder/utils to the system path
+sys.path.insert(1, 'H:/Projects/Kaggle/CZII-CryoET-Object-Identification/preprocessing')
+
+# Now you can import util
+import load
+import augment
